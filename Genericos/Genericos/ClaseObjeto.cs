0f3294ca87bbb600dc61eb692a82a425cfdca342
omar1309sm <omarsm0471@gmail.com>
2019-10-16 20:05:56 -0600

    Creada una clase de tipo object para cualquier tipo de objeto

---
+++ Genericos/Genericos/ClaseObjeto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+{
+    class ClaseObjeto
+    {
+        public ClaseObjeto(int x)
+        {
+            datoElemento = new object[x];
+        }
+
+        public void Add(Object obj)
+        {
+            datoElemento[i] = obj;
+            i++;
+        }
+
+        public Object getElemento(int x)
+        {
+            return datoElemento[x];
+        }
+
+        // array de tipo object para admitir cualquier tipo de objeto
+        private Object[] datoElemento;
+        //Contador para el array
+        private int i = 0;
+    }
+}
--- Genericos/Genericos/Form1.cs
+++ Genericos/Genericos/Form1.cs
@@ -16,5 +16,30 @@ namespace Genericos
         {
             InitializeComponent();
         }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int tamano = listBox1.Items.Count;
+
+            ClaseObjeto clase = new ClaseObjeto(tamano);
+
+            foreach (Object ob in listBox1.Items)
+            {
+                clase.Add(ob);
+            }
+
+            for (int i=0; i<tamano; i++)
+            {
+                String dat = (String)clase.getElemento(i);
+                listBox2.Items.Add(dat);
+            }
+
+        }
     }
 }
