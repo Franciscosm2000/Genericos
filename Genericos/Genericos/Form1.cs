7f4536ba1b9e1101900f7e21c0468640e60b66d8
omar1309sm <omarsm0471@gmail.com>
2019-10-16 20:42:14 -0600

    Implementando clase generica

--- Genericos/Genericos/Form1.cs
+++ Genericos/Genericos/Form1.cs
@@ -17,16 +17,18 @@ namespace Genericos
             InitializeComponent();
         }
 
-
+        //Metodo para agregar a la lista
         private void button1_Click(object sender, EventArgs e)
         {
             listBox1.Items.Add(textBox1.Text +","+ textBox2.Text);
         }
 
+        //este metodo agrega los datos a la clase Objeto y tambien imprime el objeto
         private void button2_Click(object sender, EventArgs e)
         {
             int tamano = listBox1.Items.Count;
 
+            /*
             ClaseObjeto clase = new ClaseObjeto(tamano);
 
             foreach (Object ob in listBox1.Items)
@@ -45,7 +47,27 @@ namespace Genericos
                 double sal = dat.getSalario();
                 listBox2.Items.Add(nom+","+sal);
             }
-             
+            */
+
+            Clase_Generica<Empleado> clase = new Clase_Generica<Empleado>(tamano);
+
+            foreach (Object ob in listBox1.Items)
+            {
+                String text = (String)ob;
+
+                String[] pal = text.Split(',');
+
+                clase.Add(new Empleado(pal[0], double.Parse(pal[1])));
+            }
+
+            for (int i = 0; i < tamano; i++)
+            {
+                Empleado dat = clase.getElemento(i);
+                String nom = dat.getNombre();
+                double sal = dat.getSalario();
+                listBox2.Items.Add(nom + "," + sal);
+            }
+
         }
     }
 }
